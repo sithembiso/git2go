8340f886de0d78a85e7b6fe791d7785876d6dbf1
Sithembiso Khumalo <sithembiso@lepsta.com>
2015-12-20 13:48:17 +0200

    Scripts to build static object files for libssh2 and openssl

---
+++ script/with-static-all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -ex
+
+
+export VENDOR="$PWD/vendor"
+export LIBGIT2="$VENDOR/libgit2"
+export LIBSSH2="$VENDOR/libssh2"
+export OPENSSL="$VENDOR/openssl"
+export BUILD="$VENDOR/libgit2/build"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$LIBGIT2/build:$LIBSSH2/build/src:$OPENSSL/install/lib/pkgconfig"
+
+FLAGS=$(pkg-config --static --libs --cflags libcrypto openssl libssh2 libgit2) || exit 1
+export CGO_LDFLAGS="$BUILD/libgit2.a $OPENSSL/libcrypto.a $OPENSSL/libssl.a $LIBSSH2/build/src/libssh2.a -L$BUILD ${FLAGS}"
+export CGO_CFLAGS="-I$LIBGIT2/include:$OPENSSL/include:$LIBSSH2/include"
+
+$@
